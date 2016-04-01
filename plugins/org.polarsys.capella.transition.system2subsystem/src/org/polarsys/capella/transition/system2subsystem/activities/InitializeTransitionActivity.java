405a7d36c3e4221dd9ba38819f8af8c58e59e56f
Olivier Fremion <olivier.fremion@thalesgroup.com>
2016-04-01 10:17:53 +0200

    [837] Order of Parts in Physical Architecture triggers incorrect multiphase SA-LA-PA transition Fix InitializeTransitionActivity to make sure all transitioned elements are moved to the new resource
    Bug: 837
    
    Change-Id: I215d842affe353eb1fc97a7e48272d51c1f3dfc0
    Signed-off-by: Olivier FREMION <olivier.fremion@thalesgroup.com>

--- plugins/org.polarsys.capella.transition.system2subsystem/src/org/polarsys/capella/transition/system2subsystem/activities/InitializeTransitionActivity.java
+++ plugins/org.polarsys.capella.transition.system2subsystem/src/org/polarsys/capella/transition/system2subsystem/activities/InitializeTransitionActivity.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,12 +18,11 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
-import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.polarsys.capella.core.data.capellamodeller.Project;
 import org.polarsys.capella.core.data.cs.Part;
-import org.polarsys.capella.core.model.handler.helpers.HoldingResource;
+import org.polarsys.capella.core.model.handler.helpers.HoldingResourceHelper;
 import org.polarsys.capella.core.transition.common.constants.IOptionsConstants;
 import org.polarsys.capella.core.transition.common.constants.ITransitionConstants;
 import org.polarsys.capella.core.transition.common.handlers.IHandler;
@@ -37,12 +36,12 @@ import org.polarsys.capella.transition.system2subsystem.constants.ISubSystemCons
 import org.polarsys.capella.transition.system2subsystem.handlers.attachment.FunctionalChainAttachmentHelper;
 import org.polarsys.capella.transition.system2subsystem.handlers.attachment.PhysicalPathAttachmentHelper;
 import org.polarsys.capella.transition.system2subsystem.handlers.attachment.ScenarioAttachmentHelper;
+import org.polarsys.capella.transition.system2subsystem.handlers.scope.ExternalFunctionsScopeRetriever;
 import org.polarsys.capella.transition.system2subsystem.handlers.scope.PropertyValuesScopeFilter;
 import org.polarsys.capella.transition.system2subsystem.handlers.scope.PropertyValuesScopeRetriever;
 import org.polarsys.capella.transition.system2subsystem.handlers.session.SubSystemSessionHandler;
 import org.polarsys.kitalpha.cadence.core.api.parameter.ActivityParameters;
 import org.polarsys.kitalpha.transposer.rules.handler.rules.api.IContext;
-import org.polarsys.capella.transition.system2subsystem.handlers.scope.ExternalFunctionsScopeRetriever;
 
 
 /**
@@ -58,16 +57,16 @@ public class InitializeTransitionActivity extends org.polarsys.capella.core.tran
   }
 
   @Override
-  protected IStatus initializeScopeFilterHandlers(IContext context_p, CompoundScopeFilter handler_p, ActivityParameters activityParams_p) {
-    IScopeFilter filter = PropertyValuesScopeFilter.getInstance(context_p);
-    handler_p.addScopeFilter(filter, context_p);
+  protected IStatus initializeScopeFilterHandlers(IContext context, CompoundScopeFilter handler, ActivityParameters activityParams) {
+    IScopeFilter filter = PropertyValuesScopeFilter.getInstance(context);
+    handler.addScopeFilter(filter, context);
 
-    return super.initializeScopeFilterHandlers(context_p, handler_p, activityParams_p);
+    return super.initializeScopeFilterHandlers(context, handler, activityParams);
   }
 
   @Override
-  protected Collection<Object> adaptSelection(Collection<Object> selection_p) {
-    Collection<Object> superCollection = super.adaptSelection(selection_p);
+  protected Collection<Object> adaptSelection(Collection<Object> selection) {
+    Collection<Object> superCollection = super.adaptSelection(selection);
     Collection<Object> result = new ArrayList<Object>();
     for (Object item : superCollection) {
       if (item instanceof EObject) {
@@ -84,52 +83,52 @@ public class InitializeTransitionActivity extends org.polarsys.capella.core.tran
    * {@inheritDoc}
    */
   @Override
-  protected IStatus initializeScopeRetrieverHandlers(IContext context_p, CompoundScopeRetriever handler_p, ActivityParameters activityParams_p) {
+  protected IStatus initializeScopeRetrieverHandlers(IContext context, CompoundScopeRetriever handler, ActivityParameters activityParams) {
     IScopeRetriever retriever = new PropertyValuesScopeRetriever();
-    handler_p.addScopeRetriever(retriever, context_p);
+    handler.addScopeRetriever(retriever, context);
     
-    handler_p.addScopeRetriever(new ExternalFunctionsScopeRetriever(), context_p);
+    handler.addScopeRetriever(new ExternalFunctionsScopeRetriever(), context);
     
-    return super.initializeScopeRetrieverHandlers(context_p, handler_p, activityParams_p);
+    return super.initializeScopeRetrieverHandlers(context, handler, activityParams);
   }
 
   @Override
-  protected IStatus initializeContext(IContext context_p, ActivityParameters activityParams_p) {
+  protected IStatus initializeContext(IContext context, ActivityParameters activityParams) {
 
     IHandler handler = new FunctionalChainAttachmentHelper();
-    handler.init(context_p);
-    context_p.put(ISubSystemConstants.FUNCTIONAL_CHAIN_ATTACHMENT_HELPER, handler);
+    handler.init(context);
+    context.put(ISubSystemConstants.FUNCTIONAL_CHAIN_ATTACHMENT_HELPER, handler);
 
     handler = new PhysicalPathAttachmentHelper();
-    handler.init(context_p);
-    context_p.put(ISubSystemConstants.PHYSICAL_PATH_ATTACHMENT_HELPER, handler);
+    handler.init(context);
+    context.put(ISubSystemConstants.PHYSICAL_PATH_ATTACHMENT_HELPER, handler);
 
     handler = new ScenarioAttachmentHelper();
-    handler.init(context_p);
-    context_p.put(ISubSystemConstants.SCENARIO_ATTACHMENT_HELPER, handler);
+    handler.init(context);
+    context.put(ISubSystemConstants.SCENARIO_ATTACHMENT_HELPER, handler);
 
-    return super.initializeContext(context_p, activityParams_p);
+    return super.initializeContext(context, activityParams);
   }
 
   @Override
-  protected IStatus initializeTarget(IContext context_p, ActivityParameters activityParams_p) {
+  protected IStatus initializeTarget(IContext context, ActivityParameters activityParams) {
 
     String outputproject =
-        OptionsHandlerHelper.getInstance(context_p).getStringValue(context_p, IOptionsConstants.TRANSITION_PREFERENCES, IOptionsConstants.OUTPUT_PROJECT,
+        OptionsHandlerHelper.getInstance(context).getStringValue(context, IOptionsConstants.TRANSITION_PREFERENCES, IOptionsConstants.OUTPUT_PROJECT,
             IOptionsConstants.OUTPUT_PROJECT_DEFAULT_VALUE);
     Resource outputResource = null;
 
     URI fileUri = URI.createPlatformResourceURI(outputproject, true);
 
     // we must use a single editing domain throughout the entire operation
-    EditingDomain targetDomain = (EditingDomain) context_p.get(ITransitionConstants.TRANSITION_SOURCE_EDITING_DOMAIN);
-    context_p.put(ITransitionConstants.TRANSITION_TARGET_EDITING_DOMAIN, targetDomain);
+    EditingDomain targetDomain = (EditingDomain) context.get(ITransitionConstants.TRANSITION_SOURCE_EDITING_DOMAIN);
+    context.put(ITransitionConstants.TRANSITION_TARGET_EDITING_DOMAIN, targetDomain);
 
     outputResource = targetDomain.getResourceSet().getResource(fileUri, true);
-    context_p.put(ITransitionConstants.TRANSITION_TARGET_RESOURCE, outputResource);
+    context.put(ITransitionConstants.TRANSITION_TARGET_RESOURCE, outputResource);
 
     Project project = (Project) outputResource.getContents().get(0);
-    context_p.put(ITransitionConstants.TRANSITION_TARGET_ROOT, getEngineering(project, project.getName()));
+    context.put(ITransitionConstants.TRANSITION_TARGET_ROOT, getEngineering(project, project.getName()));
 
     return Status.OK_STATUS;
   }
@@ -152,13 +151,10 @@ public class InitializeTransitionActivity extends org.polarsys.capella.core.tran
   protected IHandler createDefaultAttachmentHandler() {
     return new CapellaDefaultAttachmentHandler(){
       @Override
-      public boolean attachElementByReference(EObject sourceAttaching_p, EObject targetAttaching_p, EObject sourceAttached_p, EObject targetAttached_p,
-          EReference sourceFeature_p, EReference targetFeature_p) {
-        boolean result = super.attachElementByReference(sourceAttaching_p, targetAttaching_p, sourceAttached_p, targetAttached_p, sourceFeature_p, targetFeature_p);
-        if (((InternalEObject)targetAttached_p).eDirectResource() instanceof HoldingResource && targetAttached_p.eContainer() != null){
-          targetAttached_p.eResource().getContents().remove(targetAttached_p);
-        }
-        return result;
+      public boolean attachElementByReference(EObject sourceAttaching, EObject targetAttaching, EObject sourceAttached, EObject targetAttached,
+          EReference sourceFeature, EReference targetFeature) {
+    	  HoldingResourceHelper.ensureMoveElement(targetAttached, targetAttaching);
+    	  return super.attachElementByReference(sourceAttaching, targetAttaching, sourceAttached, targetAttached, sourceFeature, targetFeature);
       }
     };
   }
