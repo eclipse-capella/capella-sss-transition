6ddee79e6c176cc6b5494f120661955114fa7a2d
Ali AKAR <ali.akar82@gmail.com>
2020-08-07 16:42:50 +0200

    [565895] [SUBTRANS] Vertical Transition of PA to SA does not transit EI Generalization and Communication links
    Change-Id: I539c5220f265c4117af7ddfd656ecce6ad096289
    Signed-off-by: Ali AKAR <ali.akar82@gmail.com>

---
+++ plugins/org.polarsys.capella.transition.system2subsystem.crossphases/src/org/polarsys/capella/transition/system2subsystem/crossphases/rules/information/ExchangeItemElementRule.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2020 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.transition.system2subsystem.crossphases.rules.information;
+
+public class ExchangeItemElementRule
+    extends org.polarsys.capella.core.transition.system.rules.information.ExchangeItemElementRule {
+
+}
